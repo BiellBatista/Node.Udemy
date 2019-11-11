2873da9a56effeda38d8af05d39b7b78641e066a
Gabriel Batista <gabriel.texteis@gmail.com>
2019-11-10 21:30:41 -0300

    conclusão do módulo 06 da aula 01 da seção "Ecommerce Real Time com Node.js e Adonis Framework"

--- Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/01-Introducao/config/app.js
+++ Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/01-Introducao/config/app.js
@@ -1,10 +1,9 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use('Env');
 
 module.exports = {
-
   /*
   |--------------------------------------------------------------------------
   | Application Name
@@ -81,7 +80,6 @@ module.exports = {
     |
     */
     jsonpCallback: 'callback',
-
 
     /*
     |--------------------------------------------------------------------------
@@ -188,7 +186,8 @@ module.exports = {
     | Available drivers are: `file` and `console`.
     |
     */
-    transport: 'console',
+    //posso usar vários transport. Neste caso, falando para escrever em um arquivo
+    transport: 'file',
 
     /*
     |--------------------------------------------------------------------------
@@ -218,8 +217,8 @@ module.exports = {
     */
     file: {
       driver: 'file',
-      name: 'adonis-app',
-      filename: 'adonis.log',
+      name: 'ecommerce-realtime',
+      filename: 'ecommerce-error.log',
       level: 'info'
     }
   },
@@ -240,4 +239,4 @@ module.exports = {
     path: '/',
     maxAge: 7200
   }
-}
+};

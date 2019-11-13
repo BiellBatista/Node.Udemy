97d903378e84f1a46941e619c73ed2b170868c61
Gabriel Batista <gabriel.texteis@gmail.com>
2019-11-13 06:46:23 -0300

    conclusão do módulo 17 da aula 03 da seção "Ecommerce Real Time com Node.js e Adonis Framework"

--- Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/03-Models-Relacionamentos/app/Models/User.js
+++ Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/03-Models-Relacionamentos/app/Models/User.js
@@ -1,31 +1,46 @@
-'use strict'
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 
 /** @type {import('@adonisjs/framework/src/Hash')} */
-const Hash = use('Hash')
+const Hash = use('Hash');
 
 class User extends Model {
-  static boot () {
-    super.boot()
+  //teste método é chamado toda vez que um model é chamado
+  static boot() {
+    super.boot();
 
     /**
      * A hook to hash the user password before saving
      * it to the database.
      */
-    this.addHook('beforeSave', async (userInstance) => {
+    //este é um gancho que será executando quando hover um "save"(criação ou atualização)
+    this.addHook('beforeSave', async userInstance => {
+      /**
+       * Quando eu altero um campo, o dirty é modificado e com isso, o hook sabe o que alterar
+       * é estilo o modified do EFCore
+       */
       if (userInstance.dirty.password) {
-        userInstance.password = await Hash.make(userInstance.password)
+        userInstance.password = await Hash.make(userInstance.password);
       }
-    })
+    });
   }
 
-  static get traits () {
+  //ocultando o campo password em qualquer consulta ao banco
+  static get hidden() {
+    return ['password'];
+  }
+
+  /**
+   * traits me possibilita chamar um código em outro. Por exemplo, eu poderia colocar
+   * o método hidden em outro arquivo e chamá-lo na trait
+   */
+  static get traits() {
     return [
       '@provider:Adonis/Acl/HasRole',
       '@provider:Adonis/Acl/HasPermission'
-    ]
+    ];
   }
 
   /**
@@ -38,9 +53,21 @@ class User extends Model {
    *
    * @return {Object}
    */
-  tokens () {
-    return this.hasMany('App/Models/Token')
+  tokens() {
+    return this.hasMany('App/Models/Token');
+  }
+
+  image() {
+    //retorna um modelo da imagem
+    //1X1
+    return this.belongsTo('App/Models/Image');
+  }
+
+  coupons() {
+    //retorna vários coupons
+    //NXM
+    return this.belongsToMany('App/Models/Coupon');
   }
 }
 
-module.exports = User
+module.exports = User;

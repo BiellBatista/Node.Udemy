ff246dad95c019aad3bb13957dde00682a86b804
Gabriel Batista <gabriel.texteis@gmail.com>
2019-11-16 01:37:02 -0300

    conclusão do módulo 24 da aula 03 da seção "Ecommerce Real Time com Node.js e Adonis Framework"

--- Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/03-Models-Relacionamentos/app/Models/OrderItem.js
+++ Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/03-Models-Relacionamentos/app/Models/OrderItem.js
@@ -4,6 +4,14 @@
 const Model = use('Model');
 
 class OrderItem extends Model {
+  static boot() {
+    //devo chamar o boot da clase pai
+    super.boot();
+
+    //anes de salvar, devo atualizar o subtotal
+    this.addHook('beforeSave', 'OrderItemHook.updateSubtotal');
+  }
+
   static get traits() {
     return ['App/Models/Traits/NoTimestamp'];
   }

90b3703f9eb55797b346cea7816ff38782241eac
Gabriel Batista <gabriel.texteis@gmail.com>
2019-11-16 05:23:33 -0300

    conclusão do módulo 32 da aula 06 da seção "Ecommerce Real Time com Node.js e Adonis Framework"

--- Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/06-Autenticacao-Sistema/app/Controllers/Http/Auth/AuthController.js
+++ Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/06-Autenticacao-Sistema/app/Controllers/Http/Auth/AuthController.js
@@ -1,8 +1,32 @@
 'use strict';
 
+const Database = use('Database');
+const User = use('App/Models/User');
+const Role = use('Role');
+
 class AuthController {
   //auth é a autenticação do uusário (vem no header)
-  async register({ request, response }) {}
+  async register({ request, response }) {
+    const trx = await Database.beginTransaction();
+
+    try {
+      //o request.all pega retorna todos os dados
+      const { name, surname, email, password } = request.all();
+      const user = await User.create({ name, surname, email, password }, trx);
+      const userRole = await Role.findBy('slug', 'client');
+
+      await user.roles().attach([userRole.id], null, trx);
+      await trx.commit();
+
+      return response.status(201).send({ data: user });
+    } catch (error) {
+      await trx.rollback();
+
+      return response
+        .status(400)
+        .send({ message: 'Erro ao realizar o cadastro!' });
+    }
+  }
 
   async login({ request, response, atuh }) {}
 

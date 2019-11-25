307b33b05d81aad39e35afeffedf3958c6ea5d10
Gabriel Batista <gabriel.texteis@gmail.com>
2019-11-24 23:40:28 -0300

    conclusão do módulo 50 da aula 09 da seção "Ecommerce Real Time com Node.js e Adonis Framework"

--- Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/09-Users-Controller/app/Controllers/Http/Admin/UserController.js
+++ Frameworks/01-Adonis-Framework/01-Ecommerce-Real-Time/09-Users-Controller/app/Controllers/Http/Admin/UserController.js
@@ -82,7 +82,29 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params: { id }, request, response }) {
+    const user = await User.findOrFail(id);
+
+    try {
+      const userData = request.only([
+        'name',
+        'surname',
+        'email',
+        'password',
+        'image_id'
+      ]);
+
+      user.merge(userData);
+
+      await user.save();
+
+      return response.send(user);
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Não foi possível atualizar este usuário!' });
+    }
+  }
 
   /**
    * Delete a user with id.
@@ -92,7 +114,19 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const user = await User.findOrFail(id);
+
+    try {
+      await user.delete();
+
+      return response.status(204).send();
+    } catch (error) {
+      return response
+        .status(500)
+        .send({ message: 'Não foi possível deletar esse produto!' });
+    }
+  }
 }
 
 module.exports = UserController;
